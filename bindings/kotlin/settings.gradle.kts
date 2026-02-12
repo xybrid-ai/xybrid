aee3b970067a908c3fa6b9ccd9cbfecd3406703b
Glenn Sonna <kenpachi.os@gmail.com>
2026-02-12 14:36:47 +0000

    core: gradle config update

---
+++ bindings/kotlin/settings.gradle.kts
@@ -0,0 +1,21 @@
+pluginManagement {
+    plugins {
+        id("com.android.library") version "8.2.2"
+        kotlin("android") version "1.9.22"
+    }
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "xybrid-kotlin"
