c036665d1f987a04e23c61c1a4aae38adb5a65bd
Glenn Sonna <kenpachi.os@gmail.com>
2026-02-12 13:55:19 +0000

    core: update android bindings publish config

--- bindings/kotlin/build.gradle.kts
+++ bindings/kotlin/build.gradle.kts
@@ -1,8 +1,11 @@
 plugins {
     id("com.android.library")
     kotlin("android")
+    `maven-publish`
+    signing
 }
 
+group = "ai.xybrid"
 version = "0.1.0-alpha3-rc2"
 
 android {
@@ -53,4 +56,69 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+}
+
+android {
+    publishing {
+        singleVariant("release") {
+            withSourcesJar()
+        }
+    }
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("release") {
+                from(components["release"])
+
+                groupId = "ai.xybrid"
+                artifactId = "xybrid-kotlin"
+                version = project.version.toString()
+
+                pom {
+                    name.set("Xybrid Kotlin SDK")
+                    description.set("On-device ML inference for Android")
+                    url.set("https://github.com/xybrid-ai/xybrid")
+
+                    licenses {
+                        license {
+                            name.set("Apache-2.0")
+                            url.set("https://www.apache.org/licenses/LICENSE-2.0")
+                        }
+                    }
+                    scm {
+                        url.set("https://github.com/xybrid-ai/xybrid")
+                        connection.set("scm:git:git://github.com/xybrid-ai/xybrid.git")
+                    }
+                    developers {
+                        developer {
+                            id.set("xybrid-ai")
+                            name.set("Xybrid AI")
+                        }
+                    }
+                }
+            }
+        }
+
+        repositories {
+            maven {
+                name = "sonatype"
+                url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+                credentials {
+                    username = findProperty("ossrhUsername") as String? ?: ""
+                    password = findProperty("ossrhPassword") as String? ?: ""
+                }
+            }
+        }
+    }
+
+    signing {
+        val signingKey = findProperty("signingKey") as String?
+        val signingPassword = findProperty("signingPassword") as String?
+        if (signingKey != null) {
+            useInMemoryPgpKeys(signingKey, signingPassword)
+        }
+        sign(publishing.publications["release"])
+    }
 }
